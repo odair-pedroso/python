4d30e2cfde60184049659e13cc5e712abfceee81
odair-pedroso <odairpedroso90@gmail.com>
2022-11-17 21:49:40 -0300

    feat: exercicios lista

---
+++ listas_exercicios.py
@@ -0,0 +1,12 @@
+names = ["Odair", "Tamara", "Maria Eduarda", "Rosangela", "Silvana", "Matheus", "Gabriela"]
+
+print (names)
+
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+print(names[5])
+print(names[6])
+
