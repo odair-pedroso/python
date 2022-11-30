5de50a01ffa88c226d1f4fbb7129d9cd4ead8d19
odair-pedroso <odairpedroso90@gmail.com>
2022-11-30 06:36:59 -0300

    feat: exercicios lista

--- listas_exercicios.py
+++ listas_exercicios.py
@@ -10,3 +10,21 @@ print(names[4])
 print(names[5])
 print(names[6])
 
+message0 = "ola " + names[0] + " bom dia!!!!"
+message1 = "ola " + names[1] + " bom dia!!!!"
+message2 = "ola " + names[2] + " bom dia!!!!"
+message3 = "ola " + names[3] + " bom dia!!!!"
+message4 = "ola " + names[4] + " bom dia!!!!"
+message5 = "ola " + names[5] + " bom dia!!!!"
+message6 = "ola " + names[6] + " bom dia!!!!"
+
+print(message0)
+print(message1)
+print(message2)
+print(message3)
+print(message4)
+print(message5)
+print(message6)
+
+transportes = ['onibus', 'uber', '99', 'carro', 'biscicleta' ,'aviao', 'barco', 'trem', 'metro']
+print (transportes)
