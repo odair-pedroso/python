8989658a3dbf01363c3f55b5a6a3f73475033cd2
odair-pedroso <odairpedroso90@gmail.com>
2022-12-12 20:11:55 -0300

    feat: lista

---
+++ exercicios_3.4_a_3.7.py
@@ -0,0 +1,32 @@
+lista_de_convidados = ['Odair', 'Tamara', 'Maria Eduarda', 'Rosangela', 'Matheus','Vera', 'Ana','Silvana','Carlos']
+
+message0 = 'Bem vindo , convidado ' + lista_de_convidados[0]
+message1 = 'Bem vindo, convidado '+ lista_de_convidados[1]
+message2 = 'Bem vindo, convidado '+ lista_de_convidados[2]
+
+print(message0)
+print(message1)
+print(message2)
+
+print('Carlos')
+lista_de_convidados[8] = 'Marcelo'
+
+print(lista_de_convidados)
+
+message0='Bem vindo , convidado ' + lista_de_convidados[0]
+message1='Bem vindo , convidado ' + lista_de_convidados[1]
+message2='Bem vindo , convidado ' + lista_de_convidados[2]
+message3='Bem vindo , convidado ' + lista_de_convidados[3]
+message4='Bem vindo , convidado ' + lista_de_convidados[4]
+message5='Bem vindo , convidado ' + lista_de_convidados[5]
+message6='Bem vindo , convidado ' + lista_de_convidados[6]
+message7='Bem vindo , convidado ' + lista_de_convidados[7]
+
+print(message0)
+print(message1)
+print(message2)
+print(message3)
+print(message4)
+print(message5)
+print(message6)
+print(message7)
