08822bc6d04fece4de35a82237cebcea04d70b42
odair-pedroso <odairpedroso90@gmail.com>
2022-12-13 23:34:26 -0300

    feat: listas

---
+++ cars.py
@@ -0,0 +1,26 @@
+cars=['bmw','audi','toyota','subaru']
+print(cars)
+
+# ordenação de elementos de uma lista metodo sort()
+
+cars.sort()
+
+print(cars)
+
+# ordem alfabetica reversa
+cars.sort(reverse=True)
+
+print(cars)
+
+# utilizando metodo sorted() para ordenar uma lista sem que ela perca sua forma original
+cars=['bmw','audi','toyota','subaru']
+
+print('\nEsta é a lista original : ')
+print(cars)
+
+print('\nEsta é a lista com sorted :')
+print(sorted(cars))
+
+print('\nEsta é a lista original novamente :')
+print(cars)
+
