75081629f4c5cd3d7c0abfa8d3d095b8105836ac
odair-pedroso <odairpedroso90@gmail.com>
2022-12-13 21:04:33 -0300

    feat: listas exercicios

--- exercicios_3.4_a_3.7.py
+++ exercicios_3.4_a_3.7.py
@@ -47,3 +47,53 @@ lista_de_convidados.insert(4,novo_convidado_2)
 lista_de_convidados.append(novo_convidado_3)
 
 print(lista_de_convidados)
+
+message9 = "Boa noite , infelizmente só poderemos chamar 2 convidados devido ao espaço disponível."
+print (message9)
+
+convidado_excluido_1= lista_de_convidados.pop(11)
+print("Boa noite ," + convidado_excluido_1 + " sinto muito em não poder de convidar para jantar!!")
+
+convidado_excluido_2= lista_de_convidados.pop(10)
+print("Boa noite ," + convidado_excluido_2 + " sinto muito em não poder de convidar para jantar!!")
+
+convidado_excluido_3= lista_de_convidados.pop(9)
+print("Boa noite ," + convidado_excluido_3 + " sinto muito em não poder de convidar para jantar!!")
+
+convidado_excluido_4= lista_de_convidados.pop(8)
+print("Boa noite ," + convidado_excluido_4 + " sinto muito em não poder de convidar para jantar!!")
+
+convidado_excluido_5= lista_de_convidados.pop(7)
+print("Boa noite ," + convidado_excluido_5 + " sinto muito em não poder de convidar para jantar!!")
+
+convidado_excluido_6= lista_de_convidados.pop(6)
+print("Boa noite ," + convidado_excluido_6 + " sinto muito em não poder de convidar para jantar!!")
+
+convidado_excluido_7= lista_de_convidados.pop(5)
+print("Boa noite ," + convidado_excluido_7 + " sinto muito em não poder de convidar para jantar!!")
+
+convidado_excluido_8= lista_de_convidados.pop(4)
+print("Boa noite ," + convidado_excluido_8 + " sinto muito em não poder de convidar para jantar!!")
+
+convidado_excluido_9= lista_de_convidados.pop(3)
+print("Boa noite ," + convidado_excluido_9 + " sinto muito em não poder de convidar para jantar!!")
+
+convidado_excluido_10= lista_de_convidados.pop(2)
+print("Boa noite ," + convidado_excluido_10 + " sinto muito em não poder de convidar para jantar!!")
+
+print(lista_de_convidados)
+
+print("Boa noite " + lista_de_convidados[0] + " você esta convidada para o jantar!")
+print("Boa noite " + lista_de_convidados[1] + " você esta convidada para o jantar!")
+
+print(lista_de_convidados)
+
+print(lista_de_convidados[0])
+print(lista_de_convidados[1])
+
+del lista_de_convidados[0]
+print(lista_de_convidados)
+del lista_de_convidados[0]
+
+print(lista_de_convidados)
+
