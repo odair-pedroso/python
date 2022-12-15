5164db13325a18a21381d7b888626417792d5739
odair-pedroso <odairpedroso90@gmail.com>
2022-12-14 23:33:26 -0300

    feat: list exercicios

--- cars.py
+++ cars.py
@@ -1,17 +1,11 @@
 cars=['bmw','audi','toyota','subaru']
 print(cars)
-
 # ordenação de elementos de uma lista metodo sort()
-
 cars.sort()
-
 print(cars)
-
 # ordem alfabetica reversa
 cars.sort(reverse=True)
-
 print(cars)
-
 # utilizando metodo sorted() para ordenar uma lista sem que ela perca sua forma original
 cars=['bmw','audi','toyota','subaru']
 
@@ -25,14 +19,12 @@ print('\nEsta é a lista original novamente :')
 print(cars)
 
 # exibindo uma lista em ordem reversa:
-
 print (cars)
 cars.reverse()
 print(cars)
 cars.reverse()
 
 # exibindo o tamanho de uma lista
-
 len(cars)
 print(len(cars))
 
---
+++ exercicios_3.8_a_3.10.py
@@ -0,0 +1,4 @@
+# 3.8 pense em cinco lugares do mundo que vc gostaria de visitar
+
+#armazene os locais em uma lista
+
