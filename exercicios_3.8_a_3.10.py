4954872b74f85febe541c97ef3b867193b9c4662
odair-pedroso <odairpedroso90@gmail.com>
2022-12-15 21:59:34 -0300

    feat: add exercicios

--- exercicios_3.8_a_3.10.py
+++ exercicios_3.8_a_3.10.py
@@ -26,3 +26,37 @@ numero_pessoas_convidadas = str(len(convidados))
 message = 'O número de pessoas que estou convidando para o jantar são : ' + numero_pessoas_convidadas
 
 print(message)
+
+# pensar em uma lista e aplicar todos os metodos do capitulo
+
+cidades = ['Franca','São Paulo','Poços de Caldas','São Sebastião do Paraíso']
+print(cidades[0])
+
+cidades.append('Roma')
+print(cidades)
+
+cidades.insert(5,'Madrid')
+print(cidades)
+
+del(cidades[5])
+print(cidades)
+
+cidades.pop()
+print(cidades)
+print(cidades.pop())
+
+cidades.remove('Poços de Caldas')
+print(cidades)
+
+cidades=['Franca', 'São Paulo', 'Poços de Caldas', 'São Sebastião do Paraíso', 'Roma', 'Madrid']
+print(cidades)
+
+cidades.sort()
+print(cidades)
+
+cidades.sort(reverse=True)
+print(cidades)
+
+print(sorted(cidades))
+cidades.reverse()
+print(cidades)
