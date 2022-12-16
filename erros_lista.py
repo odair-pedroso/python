b3e478bf7e68d3307c732a95078afe328e881acf
odair-pedroso <odairpedroso90@gmail.com>
2022-12-16 08:47:15 -0300

    feat: add exercicios

---
+++ erros_lista.py
@@ -0,0 +1,7 @@
+# evitando erros em lista
+
+motocycles = ['honda','yamaha','suzuki']
+#print(motocycles[3])
+
+print(motocycles[-1])
+
---
+++ magicians.py
@@ -0,0 +1,17 @@
+magicians = ['alice','david','carolina']
+
+for magician in magicians:
+    print(magician)
+
+for magician in magicians:
+    print(magician.title() + ", você é um grande mágico(a)!!!")
+    print("Estamos ansiosos para ver seu próximo truque " + magician.title() + ".\n")
+print("Obrigado grandes mágicos, até a próxima!!!!")
+
+magicians = ['alice','david','carolina']
+
+for magician in magicians:
+    print(magician.title() + ", você é um grande mágico(a)!!!")
+print("Estamos ansiosos para ver seu próximo truque " + magician.title() + ".\n")
+
+
